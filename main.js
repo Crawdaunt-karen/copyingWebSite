42863c66f19062601dc4632d8f480757fe3a5615
Arakawa Madoka <arakawa.madoka@a-tm.co.jp>
2024-01-24 12:12:08 +0900

    addDotPagenation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let slideIndex = 0; // 現在のスライドインデックスを保持
+let dots = document.getElementsByClassName("dot");
 let slides = document.getElementsByClassName("slide");
 let prev = document.querySelector(".prev");
 let next = document.querySelector(".next");
@@ -30,12 +31,31 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   // 現在のslideIndexのみ表示
   slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
+}
+
+// ドットがクリックされたら対応するスライドを表示する関数
+function currentSlide(n) {
+  showSlides((slideIndex = n - 1));
+}
+
+// アクティブなドットにスタイルを適用する関数
+function updateDots() {
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex].className += " active";
 }
 
 // 初期表示のスライドを指定
 showSlides(slideIndex);
+// 初期化事にドットを更新
+updateDots();
 
 // 前後コントロールのイベントリスナーを追加
 prev.addEventListener("click", showPrevSlide);
