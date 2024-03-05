78552af57a011086a3cd27283c8352b47f065b75
Arakawa Madoka <arakawa.madoka@a-tm.co.jp>
2024-03-05 15:34:24 +0900

    headerAndMainvisual

--- companySite/src/main.js
+++ companySite/src/main.js
@@ -1,13 +1,3 @@
 $(function () {
-  const currentPath = window.location.pathname;
-  console.log(currentPath);
-
-  $(".menu-list-item a").each(function () {
-    const href = $(this).attr("href"); // 各アンカーのhref属性を取得
-    // 現在のURLとhref属性を比較し、一致する場合にスタイルを変更
-    if (href === currentPath) {
-      console.log(href);
-      $(this).addClass("active-link");
-    }
-  });
+  $(".menu-list-item:first-child a").addClass("active-link");
 });
