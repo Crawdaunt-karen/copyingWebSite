24512b279c0661399c5be870d1389c006e3dddf0
Arakawa Madoka <arakawa.madoka@a-tm.co.jp>
2024-03-06 17:43:21 +0900

    spversion

--- companySite/src/main.js
+++ companySite/src/main.js
@@ -1,3 +1,9 @@
 $(function () {
   $(".menu-list-item:first-child a").addClass("active-link");
+
+  // SP版のハンバーガーメニューのクラスtoggle
+  $("#header-menu-btn").click(() => {
+    $(".menu-list").toggleClass("active");
+    $("#header-menu-btn").toggleClass("active"); // メニューボタンにもactiveクラスをトグル
+  });
 });
