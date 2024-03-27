81b33538522b58a5bb564792867cbdeaf8b4e829
Arakawa Madoka <arakawa.madoka@a-tm.co.jp>
2024-03-27 14:00:04 +0900

    finishSP

--- engbody/src/main.js
+++ engbody/src/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+  // stickyヘッダー
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    const defaultHeader = document.querySelector(".default-header");
+    const scrolledContent = document.querySelector(".header-scrolled");
+
+    if (window.scrollY > 0) {
+      header.classList.add("header-fixed");
+      scrolledContent.style.display = "block";
+      defaultHeader.style.display = "none";
+    } else {
+      header.classList.remove("header-fixed");
+      scrolledContent.style.display = "none";
+      defaultHeader.style.display = "block";
+    }
+  });
+
   $(".tablinks").click(function () {
     var tabId = $(this).data("tab");
     $(".tabcontent").css("display", "none");
@@ -9,17 +26,44 @@ $(document).ready(function () {
 
   $(".tablinks:first").trigger("click");
 
-  //  studioのスライダー
-  $(".studio-slider").slick({
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
+  const studioSwiper = new Swiper(".studio-swiper-container", {
+    // Swiperのオプション
+    loop: true,
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true, // ドットをクリック可能にする
+    },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
   });
 
-  // priceのスライダー
-  $(".price-slider").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
+  const priceSwiper = new Swiper(".price-swiper-container", {
+    // Swiperのオプション
+    loop: true,
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 20,
+    // スクロールバー
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+    },
+  });
+
+  details.querySelector("summary").addEventListener("click", function (evt) {
+    const icon = this.querySelector(".material-icons");
+    if (details.open) {
+      icon.textContent = "expand_more";
+    } else {
+      // Timeoutを追加して、detailsの状態が切り替わるのを待つ
+      setTimeout(() => {
+        icon.textContent = "expand_more";
+      }, 1);
+    }
   });
 });
